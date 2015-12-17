c9b73d222d6df06ec428b37ff3046686665f1d11
Joea <pangwenjing@gmail.com>
2015-12-17 10:06:40 +0800

    Knapsack
    Create the Knapsack

---
+++ Unity/NGUI base on unity4.6/Assets/Scripts/KnapsackItem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnapsackItem :UIDragDropItem {
+
+    protected override void OnDragDropStart ( )
+    {
+        base.OnDragDropStart ( );
+        //设置物品的深度，防止拖拽时被遮挡
+        this.transform.GetComponent<UISprite> ( ).depth = 4;
+    }
+
+
+    //重写OnDragDropRelease方法，实现物品的位置交换
+    protected override void OnDragDropRelease ( GameObject surface )
+    {
+        base.OnDragDropRelease ( surface );
+        //如果是cell，说明格子是空的
+        if (surface.tag == "Cell")
+        {
+            this.transform.parent = surface.transform;
+            this.transform.localPosition = Vector3.zero;
+        }
+        //如果是Knapsack_item，说明格子中存在物品
+        if (surface.tag == "Knapsack_item") {
+            Transform tempParent = surface.transform.parent;
+            surface.transform.parent=this.transform.parent;
+            surface.transform.localPosition = Vector3.zero;
+            this.transform.parent = tempParent;
+            this.transform.localPosition = Vector3.zero;
+
+        }
+        //重置物品的深度
+        this.transform.GetComponent<UISprite> ( ).depth = 3;
+        
+    }
+
+
+
+}
---
+++ Unity/NGUI base on unity4.6/Assets/Scripts/KnapsackManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnapsackManager : MonoBehaviour
+{
+
+
+    public GameObject[] cells;
+    GameObject item;
+    void Update ( )
+    {
+        if (Input.GetKeyDown ( KeyCode.Space ))
+        {
+            Debug.Log ( "space" );
+            PickUp ( );
+        }
+    }
+
+    //模拟拾取物品
+    public string[] ItemName;
+    public void PickUp ( )
+    {
+        item = Resources.Load ( "Knapsack_item" ) as GameObject;//加载Prefabs
+        GameObject go= GameObject.Instantiate ( item ) as GameObject;//实例化Prefabs
+        for(int i=0;i<cells.Length;i++){
+            if (cells[i].transform.childCount == 0) {
+                
+                go.transform.parent = cells[i].transform;
+                go.transform.localScale = Vector3.one;
+                go.transform.localPosition = Vector3.zero;
+                break;
+
+            }
+        }
+        
+        
+
+    }
+}
